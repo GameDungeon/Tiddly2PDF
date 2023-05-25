837e3d08142b331f00cebc6344394e10f2a5602e
gamedungeon <gamedungeon@protonmail.com>
2023-05-25 17:51:50 -0500

    Basic PDF Support

---
+++ credits.md
@@ -0,0 +1,5 @@
+# Credits
+
+Remember to move these into the plugin.
+
+PDF by Tim Neumann from <a href="https://thenounproject.com/browse/icons/term/pdf/" target="_blank" title="PDF Icons">Noun Project</a>
---
+++ src/TiddlyPDF/WidgetLoader.js
@@ -0,0 +1,16 @@
+// https://talk.tiddlywiki.org/t/using-external-js-libraries-in-tw-plugins/5893
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+(function exportPDFWidgetLoader() {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!$tw.browser) {
+      return;
+    }
+    // separate the widget from the exports here, so we can skip the require of react code if `!$tw.browser`. Those ts code will error if loaded in the nodejs side.
+    const components = require('$:/plugins/GameDungeon/TiddlyPDF/ExportAsPDFWidget.js');
+    const { exportAsPDF } = components;
+
+    /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+    exports.exportAsPDF = exportAsPDF;
+    exports["action-export-pdf"] = exportAsPDF;
+})();
